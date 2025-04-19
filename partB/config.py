66355190d318b902c83adf70c5f341ce33aa2c92
Rajshekhar Rakshit <44888331+DextroLaev@users.noreply.github.com>
2025-04-19 21:05:08 +0530

    Update config.py

--- partB/config.py
+++ partB/config.py
@@ -1,3 +1,18 @@
 import torch
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+OUT_CLASSES = 10
+
+DATA_AUG = False
+BATCH_SIZE = 32
+
+LEARNING_RATE = 1e-3
+WEIGHT_DECAY = 0.0
+EPOCHS = 25
+LOG_WANDB = True
+SAVE_MODEL = True
+EARLY_STOPPING_PATIENCE = 10
+
+TRAIN_METHOD = 'last_layerpu'
+TRAIN_LAST_K_LAYERS = 3
+TRAIN_NEW_FC_STEP = 2
